0698a71ced86774a37c1f0ac5983d579a8529b79
Debopam Basu <debopamb@gmail.com>
2020-05-20 22:19:14 +0530

    Adding functionality to add left nav items

--- src/extensions/injectCss/InjectCssApplicationCustomizer.ts
+++ src/extensions/injectCss/InjectCssApplicationCustomizer.ts
@@ -20,13 +20,23 @@ export default class InjectCssApplicationCustomizer
   public onInit(): Promise<void> {
     Log.info(LOG_SOURCE, `Initialized ${strings.Title}`);   
 
-    var coll = document.getElementsByTagName("a");
-    for (var i = 0; i < coll.length; i++) {
-    if(coll[i].title === "Home"){
-        coll[i].href = "https://www.bing.com"
-    }
-  }
+  //   var coll = document.getElementsByTagName("a");
+  //   for (var i = 0; i < coll.length; i++) {
+  //   if(coll[i].title === "Home"){
+  //       coll[i].href = "https://www.bing.com"
+  //   }
+  // }
 
+      var lists = document.getElementsByTagName("nav");
+      var li = document.createElement('li');
+      li.innerHTML = '<div name="new link added" class="ms-Nav-compositeLink compositeLink-130"><a class="ms-Button ms-Button--action ms-Button--command ms-Nav-link link-139" href="https://www.bing.com" title="new link added" data-is-focusable="true" tabindex="0"><span class="ms-Button-flexContainer flexContainer-87" data-automationid="splitbuttonprimary"><div class="ms-Nav-linkText linkText-70">new link added</div></span></a></div>'; 
+      li.className = 'ms-Nav-navItem navItem-75';
+      for (var i = 0; i < lists.length; ++i) {
+              var items = lists[i].getElementsByTagName("ul");
+              for (var j = 0; j < 1; ++j) {
+                  items[j].prepend(li);
+              }          
+      }
     return Promise.resolve();
   }
 }
